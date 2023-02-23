5140244ac793deba6d3c77b968286c4f101a176d
underthemoss <will@equipmentshare.com>
2023-02-23 09:54:45 -0600

    push3

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import physicalProducts from "../data/physicalProducts.json";
 import _serviceProducts from "../data/serviceProducts.json";
-import RightSidebar from "./rightSideBar";
 
 function ProductDetails({ selectedProduct }) {
   const [worksWith, setWorksWith] = useState([]);
